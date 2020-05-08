818d60022c6d8d63415049da0d54e8808590076a
Konrad982 <konrad-dierks@gmx.de>
2020-05-08 16:52:40 +0200

    added saving according to dataset

--- covid19_inference/data_collection.py
+++ covid19_inference/data_collection.py
@@ -3,17 +3,23 @@ import numpy as np
 import os
 from covid19_inference import plotting
 
-def update_collection(country, trace=None,varnames=None, change_points=None, other_vars=None):
+def update_collection(country, dataset="unknown", trace=None,varnames=None, change_points=None, other_vars=None):
     """
     Saves the given data into a directory
     
     Parameters
     ----------
     filename: str
+    dataset: str
     country: str
     trace: pymc3 multitrace
+    varnames: put something that is conversible to str, e.g. model.unobserved_RVs for a pymc3 model
     change_points: dictionary
-    other_vars: dictionary, make sure to include the variables that you need later
+    other_vars: dictionary, make sure to include the variables that you need later, e.g.
+    other_vars = {"bd": bd, "ed": ed, 
+              "diff_data_sim": diff_data_sim, "num_days_forecast": num_days_forecast, 
+              "lockdown_date": lockdown_date, "lockdown_type": lockdown_type}
+    in the usual scripts
     
     Returns
     -------
@@ -30,6 +36,12 @@ def update_collection(country, trace=None,varnames=None, change_points=None, oth
     except FileNotFoundError:
         os.mkdir(country)
         os.chdir(country)
+        
+    try:
+        os.chdir(dataset)
+    except FileNotFoundError:
+        os.mkdir(dataset)
+        os.chdir(dataset)
         
     for varname in varnames:
         try:
@@ -61,32 +73,66 @@ def update_collection(country, trace=None,varnames=None, change_points=None, oth
     
     os.chdir("..")
     os.chdir("..")
+    os.chdir("..")
     
     return
     
-def read_variable(country, variable):
+def read_variable(country, dataset, variable):
     try:
         os.chdir("results_collection")
     except FileNotFoundError:
-        os.mkdir("results_collection")
-        os.chdir("results_collection")
+        print("Collection not found")
+        return
     
     try:
         os.chdir(country)
     except FileNotFoundError:
         print("Country not found")
+        os.chdir("..")
+        return
+    
+    try:
+        os.chdir(dataset)
+    except FileNotFoundError:
+        print("Dataset not found")
+        os.chdir("..")
+        os.chdir("..")
         return
         
     try:
         df = pd.read_csv(variable)
-        os.chdir("..")
-        os.chdir("..")
-        return df
         
     except FileNotFoundError:
         print("variable not found")
-        os.chdir("..")
-        os.chdir("..")
-    
+        df = None
+        
+    os.chdir("..")
+    os.chdir("..")
+    os.chdir("..")
+    return df
+
+def read_variable_country(country, variable):
+    try:
+        os.chdir("results_collection")
+    except FileNotFoundError:
+        print("Collection not found")
         return
+    
+    try:
+        os.chdir(country)
+    except FileNotFoundError:
+        print("Country not found")
+        os.chdir("..")
+        return
+    
+    datasets = os.listdir()
+    os.chdir("..")
+    os.chdir("..")
+
+    
+    dfdict = {}
+    for dataset in datasets:
+        dfdict[dataset] = read_variable(country, dataset, variable)
+    
+    return dfdict
     
