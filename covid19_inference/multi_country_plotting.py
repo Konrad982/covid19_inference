113fb7874b3b827d85cd4cec1c5f90f07745a52e
Konrad982 <konrad-dierks@gmx.de>
2020-05-19 18:41:46 +0200

    Some updates, especially comparison

--- covid19_inference/multi_country_plotting.py
+++ covid19_inference/multi_country_plotting.py
@@ -212,13 +212,13 @@ def plotly_violins_all_available_lambda(distances=5, around="lockdown", refdate=
                 ).strftime("%d/%m/%Y")
 
             if datasets[i] == "hospitalisations":
-                name_0 = country + " new_hosp " + date_0_string
-                name_1 = country + " new_hosp " + date_1_string
+                name_0 = country + " hosp " + date_0_string
+                name_1 = country + " hosp " + date_1_string
                 md_table = (
                     md_table
                     + "|"
                     + country
-                    + "|new_hosp|"
+                    + "|hosp|"
                     + lddate_normalstring
                     + "|"
                     + ldtype
@@ -338,7 +338,8 @@ def plotly_violins_all_available_lambda(distances=5, around="lockdown", refdate=
             )
 
     md_table = (
-        md_table + "\n For the links to the sources, see the readme of the repository.\n\n"
+        md_table
+        + "\n For the links to the sources, see the readme of the repository.\n\n"
     )
     md_table = (
         md_table
@@ -365,4 +366,178 @@ def plotly_violins_all_available_lambda(distances=5, around="lockdown", refdate=
     #        ),
     #        name ="mu"
     # )
+    return figure
+
+
+def plotly_violins_all_available_ratio(distances=5, around="lockdown", refdate=None):
+    # if distances is an integer, make a tuple.
+    if isinstance(distances, int):
+        distances = (distances, distances)
+
+    # tell what's going on
+    display(
+        Markdown(
+            "# Violin plots of the ratio of $\lambda$ for each of the countries / datasets found \n"
+            + str(distances[1])
+            + " days after and "
+            + str(distances[0])
+            + " days before lockdown\n"
+        )
+    )
+
+    # get country names in results_collection folder
+    countries = data_collection.get_countries()
+
+    traces = []
+    current = 0
+    # we assume generally mu=0.125
+    mu = 0.125
+    printhosp = False
+    printnew_gvt = False
+    printJHU = False
+
+    md_table = "|Country|Dataset|lockdown date |lockdown type|data retrieved|\n|:---|:---|:---|:---|:---|\n"
+    for country in countries:
+        other_vars = data_collection.read_variable_country(country, "other_vars")
+        lambda_t = data_collection.read_variable_country(country, "lambda_t")
+
+        # get dataset names
+        datasets = list(other_vars.keys())
+
+        for i in range(0, len(datasets)):
+            bd = other_vars[datasets[i]]["bd"].to_numpy()[0]
+            diff_data_sim = other_vars[datasets[i]]["diff_data_sim"].to_numpy()[0]
+
+            if around == "lockdown":
+                lddate = other_vars[datasets[i]]["lockdown_date"].to_numpy()[0]
+                ldtype = other_vars[datasets[i]]["lockdown_type"].to_numpy()[0]
+                lddate_normalstring = datetime.datetime.strptime(
+                    lddate, "%Y-%m-%d"
+                ).strftime("%d/%m/%Y")
+
+                ld_num = (
+                    datetime.datetime.strptime(lddate, "%Y-%m-%d")
+                    - datetime.datetime.strptime(bd, "%Y-%m-%d")
+                ).days + diff_data_sim
+
+                diff_0 = ld_num - distances[0]
+                diff_1 = ld_num + distances[1]
+
+            if datasets[i] == "hospitalisations":
+                name = country + " " + "hosp" + " around " + lddate_normalstring
+                md_table = (
+                    md_table
+                    + "|"
+                    + country
+                    + "|hosp|"
+                    + lddate_normalstring
+                    + "|"
+                    + ldtype
+                )
+                printhosp = True
+            elif datasets[i] == "new_cases_government_data":
+                name = country + " " + "new_gvt" + " around " + lddate_normalstring
+                md_table = (
+                    md_table
+                    + "|"
+                    + country
+                    + "|new_gvt|"
+                    + lddate_normalstring
+                    + "|"
+                    + ldtype
+                )
+                printnew_gvt = True
+            elif datasets[i] == "new_lab":
+                name = country + " " + "new_lab" + " around " + lddate_normalstring
+                md_table = (
+                    md_table
+                    + "|"
+                    + country
+                    + "|new_lab|"
+                    + lddate_normalstring
+                    + "|"
+                    + ldtype
+                )
+                printnew_lab = True
+            else:
+                if datasets[i] == "JHU":
+                    printJHU = True
+                name = country + " " + datasets[i] + " around " + lddate_normalstring
+                md_table = (
+                    md_table
+                    + "|"
+                    + country
+                    + "|"
+                    + datasets[i]
+                    + "|"
+                    + lddate_normalstring
+                    + "|"
+                    + ldtype
+                )
+
+            if "data_retrieval" in list(other_vars[datasets[i]].keys()):
+                md_table = (
+                    md_table
+                    + "|"
+                    + other_vars[datasets[i]]["data_retrieval"].to_numpy()[0]
+                    + "|\n"
+                )
+            else:
+                md_table = md_table + "| |\n"
+
+            # x_0 = [date_0_string for i in range(0, len(lambda_t[datasets[i]].to_numpy()[:,diff_0]))]
+            # x_1 = [date_1_string for i in range(0, len(lambda_t[datasets[i]].to_numpy()[:,diff_1]))]
+
+            raw_data = lambda_t[datasets[i]].to_numpy()[:, diff_0]
+            raw_data_2 = lambda_t[datasets[i]].to_numpy()[:, diff_1]
+            quotient = np.divide(raw_data, raw_data_2)
+            processed_quotient = quotient[
+                np.logical_and(
+                    np.quantile(quotient, 0.05) < quotient,
+                    quotient < np.quantile(quotient, 0.95),
+                )
+            ]
+            traces.append(
+                go.Violin(
+                    y=processed_quotient,
+                    name=name,
+                    legendgroup=name,
+                    meanline_visible=True,
+                    box={"visible": False},
+                    points=False,
+                )
+            )
+            # traces.append(go.Scatter(x=[current, current + 1], y=[mu, mu], legendgroup = name_1, mode="lines", line=dict(color="black")))
+    display(Markdown(md_table))
+    md_table = ""
+    if printJHU or printnew_gvt or printhosp or printnew_lab:
+        md_table = "|Abbreviation|Meaning| \n|:---|:---|\n"
+        if printJHU:
+            md_table = md_table + "|JHU|Data from Johns Hopkins university|\n"
+        if printnew_gvt:
+            md_table = (
+                md_table
+                + "|new_gvt|New cases according to government data of the respective country|\n"
+            )
+        if printhosp:
+            md_table = (
+                md_table
+                + "|hosp|New hospitalisations according to government data of the respective country|\n"
+            )
+        if printnew_lab:
+            md_table = (
+                md_table
+                + "|new_lab|France-specific, positive tests of a part of the laboratories (*laboratoires de ville*)|\n"
+            )
+
+    md_table = (
+        md_table
+        + "\n For the links to the sources, see the readme of the repository.\n\n"
+    )
+    md_table = (
+        md_table
+        + "__After calculating the ratio, only the ratios between the 0.05 and 0.95 quantiles are plotted. The following plots are interactive, by clicking on a legend item, the corresponding plot will disappear. Double-click to isolate.__\n\n"
+    )
+    display(Markdown(md_table))
+    figure = go.Figure(data=traces, layout={"title": {"text": r""}, "height": 700,},)
     return figure
